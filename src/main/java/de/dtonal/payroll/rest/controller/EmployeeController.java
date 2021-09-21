21285e37f34bef525f36dae5e1f34f1650b9d097
dtonal <dtonal@posteo.de>
2021-09-21 20:50:24 +0200

    Adding hateoas

--- src/main/java/de/dtonal/payroll/rest/controller/EmployeeController.java
+++ src/main/java/de/dtonal/payroll/rest/controller/EmployeeController.java
@@ -1,7 +1,13 @@
 package de.dtonal.payroll.rest.controller;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,13 +28,26 @@ public class EmployeeController {
 	}
 
 	@GetMapping("/employees")
-	public List<Employee> all() {
-		return repository.findAll();
+	CollectionModel<EntityModel<Employee>> all() {
+
+		List<EntityModel<Employee>> employees = repository.findAll().stream()
+				.map(employee -> EntityModel.of(employee,
+						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+				.collect(Collectors.toList());
+
+		return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
 	}
 
 	@GetMapping("/employees/{id}")
-	public Employee find(@PathVariable Long id) {
-		return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+	public EntityModel<Employee> one(@PathVariable Long id) {
+
+		Employee employee = repository.findById(id) //
+				.orElseThrow(() -> new EmployeeNotFoundException(id));
+
+		return EntityModel.of(employee, //
+				linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
 	}
 
 	@DeleteMapping("/employees/{id}")
